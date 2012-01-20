5c857d960f60e8016171890b5121d9a6226691e0
Scott Heckel <scott.heckel@gmail.com>
2012-01-20 01:29:07 -0600

    Very rough working version of displaying today's featured shows from tv rage

---
+++ MobileTVGuide/Models/Guide.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileTVGuide.Models
+{
+    /// <summary>
+    /// Represents all Guide data
+    /// </summary>
+    public class Guide
+    {
+        /// <summary>
+        /// Channels listed in the Guide
+        /// </summary>
+        public IDictionary<string, Channel> Channels
+        {
+            get;
+            internal set;
+        }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Guide()
+        {
+            Channels = new Dictionary<string, Channel>();
+        }
+    }
+}
---
+++ MobileTVGuide/Models/Show.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileTVGuide.Models
+{
+    public class Show
+    {
+        /// <summary>
+        /// Show Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Start Time
+        /// </summary>
+        public string StartTime { get; set; }
+    }
+}
