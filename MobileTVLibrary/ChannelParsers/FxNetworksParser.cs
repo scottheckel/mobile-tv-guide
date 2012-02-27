a1ddddfa41d732a690d4cfa17204636ebb1ae899
Scott Heckel <scott.heckel@gmail.com>
2012-02-27 14:44:58 -0600

    Adding in the running length and show name for FX, which are both rather fragile.  The show name will correctly parse out movie names.

--- MobileTVLibrary/ChannelParsers/FxNetworksParser.cs
+++ MobileTVLibrary/ChannelParsers/FxNetworksParser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
 using MobileTVLibrary.Helpers;
 using MobileTVLibrary.Models;
 using MobileTVLibrary.Services.Channels;
@@ -16,8 +17,8 @@ namespace MobileTVLibrary.ChannelParsers
         /// <summary>
         /// Retrieve the Show list based on the provided service
         /// </summary>
-        /// <param name="service"></param>
-        /// <returns></returns>
+        /// <param name="service">Fx Network Channel Service</param>
+        /// <returns>A list of shows</returns>
         public IEnumerable<Show> RetrieveShows(IChannelService service)
         {
             var doc = service.Get();
@@ -27,15 +28,54 @@ namespace MobileTVLibrary.ChannelParsers
                                    {
                                        ChannelName = "FX",
                                        Description = "",
-                                       Name = showRow.QuerySelector("strong").InnerText,
-                                       StartTime = showRow.QuerySelector("p:first-child").InnerText
+                                       Name = DetermineShowName(showRow),
+                                       StartTime = showRow.QuerySelector("p:first-child").InnerText,
+                                       Length = DetermineLengthFromStyleWidth(showRow.Attributes["style"].Value)
                                    };
 
-            // TODO: name won't work right with movies
             // TODO: parse out the date on the start time
             // TODO: Determine show length, will need to do it off the width of the li's (I know it's weird)
+            // TODO: Description would be a network call to the show description page at a url like this http://www.fxnetworks.com/get_show_detail.php?show_code=429646&show_time=Tue%20Feb%2028th%2011:00pm
 
             return shows;
+        }
+
+        /// <summary>
+        /// Determine the time length of the show
+        /// </summary>
+        /// <param name="styleAttributeValue">Style attribute's content value</param>
+        /// <returns>The running length</returns>
+        private int DetermineLengthFromStyleWidth(string styleAttributeValue)
+        {
+            // HACK: Using display values to get a show length... yummy
+            int pixelWidthStart = styleAttributeValue.IndexOf("width:", StringComparison.CurrentCulture) + 6;
+            string pixelWidthText = styleAttributeValue.Substring(pixelWidthStart, styleAttributeValue.IndexOf("px", pixelWidthStart, StringComparison.CurrentCulture) - pixelWidthStart);
+            
+            int pixelWidth;
+            if (int.TryParse(pixelWidthText, out pixelWidth))
+            {
+                // 30 minutes is 176 pixels right now
+                return (pixelWidth/176)*30;
+            }
+
+            return 30;
+        }
+
+        /// <summary>
+        /// Determine the show name, this is needed to parse out the FX Movie and get the actual name 
+        /// </summary>
+        /// <param name="showRow">Show Row's HTML Node</param>
+        /// <returns>Show/Movie Name</returns>
+        private string DetermineShowName(HtmlNode showRow)
+        {
+            var showName = showRow.QuerySelector("strong").InnerText;
+            if (showName == "FX Movie")
+            {
+                // HACK: Yeah, this is a bit of a hack
+                showName = showRow.QuerySelector("span p").InnerHtml;
+                showName = showName.Substring(showName.IndexOf("</strong>&nbsp;", StringComparison.CurrentCulture) + 15);
+            }
+            return showName;
         }
 
         private string DetermineShowRowSelector(DateTime requestedDate)
